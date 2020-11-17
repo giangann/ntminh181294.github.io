2842718105a645d7461ddc726bcffe0cc8fca8c0
ntminh181294 <73157570+ntminh181294@users.noreply.github.com>
2020-11-17 23:00:26 +0700

    baitapbuoi11

---
+++ buoi11/script.js
@@ -0,0 +1,22 @@
+// câu 1
+function tong(x, y) {
+  alert("tổng: " + (x + y));
+}
+function toanbo(x, y) {
+  alert("tổng: " + (x + y));
+  alert("hiệu: " + (x - y));
+  alert("nhân: " + x * y);
+  alert("chia: " + x / y);
+  alert("dư: " + (x % y));
+  alert("mũ: " + x ** y);
+}
+function chuvi(x, y) {
+  alert("chu vi: " + (x + y) * 2);
+}
+function dientich(x, y) {
+  alert("diện tích: " + x * y);
+}
+function hinhtron(x) {
+  alert("chu vi hình tròn: " + 2 * 3.14 * x);
+  alert("diện tích hình tròn: " + 3.14 * x ** 2);
+}
