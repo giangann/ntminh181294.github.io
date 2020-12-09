93f9927bfc807f54eb8bdc8aabd4b3fb1c327bf7
ntminh181294 <73157570+ntminh181294@users.noreply.github.com>
2020-12-09 21:32:46 +0700

    Update script.js

--- jsDOM/script.js
+++ jsDOM/script.js
@@ -1,18 +1,37 @@
-function changeColor() {
-  document.getElementById("p1").style.color = "blue";
-  document.getElementById("p2").style.color = "yellow";
-  document.getElementById("p3").style.color = "red";
-}
-function changeBgColor(color) {
-  document.body.style.backgroundColor = color;
-}
-function copyContent(paragraph1, paragraph2) {
-  document.getElementById(paragraph1).innerHTML = document.getElementById(
-    paragraph2
-  ).innerHTML;
-}
-function changeFontSize(x) {
-  document.body.style.fontSize = x;
-  //   document.getElementById("p2").style.fontSize = x;
-  //   document.getElementById("p3").style.fontSize = x;
+// function changeColor() {
+//   document.getElementById("p1").style.color = "blue";
+//   document.getElementById("p2").style.color = "yellow";
+//   document.getElementById("p3").style.color = "red";
+// }
+// function changeBgColor(color) {
+//   document.body.style.backgroundColor = color;
+// }
+// function copyContent(paragraph1, paragraph2) {
+//   document.getElementById(paragraph1).innerHTML = document.getElementById(
+//     paragraph2
+//   ).innerHTML;
+// }
+// function changeFontSize(x) {
+//   let arr = document.getElementsByTagName("p");
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i].style.fontSize = x + "px";
+//   }
+// }
+// function reverseContent(p1, p2) {
+//   let paragraph1 = document.getElementById(p1);
+//   let paragraph2 = document.getElementById(p2);
+//   let temp = paragraph1.innerHTML;
+//   paragraph1.innerHTML = paragraph2.innerHTML;
+//   paragraph2.innerHTML = temp;
+// }
+// function increaseFontSize() {
+//   let paragraph1 = document.getElementById(p1);
+//   let p1FontSize = paragraph1.style.fontSize;
+//   l;
+// }
+function swapPlace() {
+  let arr = document.getElementsByTagName("iframe");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].style.left = "12px";
+  }
 }
