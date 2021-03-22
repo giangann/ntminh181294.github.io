04d0f913025ebc510033093f1aa00950c0346e1a
ntminh181294 <73157570+ntminh181294@users.noreply.github.com>
2021-03-22 15:25:41 +0700

    22.3.2021

---
+++ public/script.js
@@ -0,0 +1,30 @@
+function loadDoc() {
+  let xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let users = JSON.parse(this.responseText);
+      let content = "";
+      for (let i = 0; i < users.length; i++) {
+        content += `<tr>
+          <td>${users[i].name}</td>
+          <td>${users[i].birthday.substr(-4)}</td>
+          <td>${users[i].email}</td>
+          <td>${users[i].phone}</td>
+          <td>
+                <a href="#" class="edit"><i class="fas fa-edit"></i>Chỉnh sửa</a
+                ><span>|</span
+                ><a href="#" class="delete"
+                  ><i class="fas fa-trash-alt"></i>Xóa</a
+                >
+              </td>
+        </tr>`;
+      }
+      $("tbody").html(content);
+    }
+  };
+  xhttp.open("GET", "http://localhost:3000/users", true);
+  xhttp.send();
+}
+
+loadDoc();
