1c60a781b8a2a561938e5e62023d3971791d1fbc
ntminh181294 <73157570+ntminh181294@users.noreply.github.com>
2021-03-24 19:06:27 +0700

    24.3.2021

--- public/script.js
+++ public/script.js
@@ -8,46 +8,16 @@ function loadDoc() {
       for (let i = 0; i < users.length; i++) {
         content += `<tr>
           <td>${users[i].name}</td>
-          <td>${users[i].birthday.substr(-4)}</td>
+          <td>${users[i].birthday}</td>
           <td>${users[i].email}</td>
           <td>${users[i].phone}</td>
           <td>
-                <button data-toggle="modal" data-target="#myModal" class="edit">
-                  <i class="fas fa-edit"></i>Chỉnh sửa
-                </button>
-                <div id="myModal" class="modal fade" role="dialog">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <form>
-                        <div class="form-input">
-                          <label for="name">Họ tên<span>*</span></label>
-                          <input type="text" id="name" name="name" />
-                        </div>
-                        <div class="form-input">
-                          <label for="birthday">Năm sinh</label>
-                          <input
-                            type="date"
-                            id="birthday"
-                            name="birthday"
-                          />
-                        </div>
-                        <div class="form-input">
-                          <label for="email">Email<span>*</span></label>
-                          <input type="email" id="email" name="email" />
-                        </div>
-                        <div class="form-input">
-                          <label for="phone">Phone<span>*</span></label>
-                          <input type="tel" id="phone" name="phone" />
-                        </div>
-                        <button>Sửa</button>
-                      </form>
-                    </div>
-                  </div>
-                </div><span>|</span
-                ><button class="delete" onclick='productDelete(this)'
-                  ><i class="fas fa-trash-alt"></i>Xóa</button
+                <a href="#" class="edit"><i class="fas fa-edit"></i>Chỉnh sửa</a
+                ><span>|</span
+                ><a href="#" class="delete"
+                  ><i class="fas fa-trash-alt"></i>Xóa</a
                 >
-          </td>
+              </td>
         </tr>`;
       }
       $("tbody").html(content);
@@ -58,9 +28,3 @@ function loadDoc() {
 }
 
 loadDoc();
-
-function productDelete(ctl) {
-  $(ctl).parents("tr").remove();
-}
-productDelete(ctl);
-
